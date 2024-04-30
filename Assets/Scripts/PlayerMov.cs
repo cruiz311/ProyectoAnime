cd4821b08237f55a0d4dd36defe1afbd0dd369c0
cruiz311 <74808642+cruiz311@users.noreply.github.com>
2024-04-29 23:28:13 -0500

    ANIM

---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    public bool isFacingRight = false;
+    public float moveSpeed = 5f;
+    public Animator anim;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        
+        if(isFacingRight && movement.x < 0f || !isFacingRight && movement.x > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
+
+
+
+
+
+
