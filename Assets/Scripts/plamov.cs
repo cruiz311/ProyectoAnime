878515dd777ab6ef5473500c99673c706d491443
cruiz311 <74808642+cruiz311@users.noreply.github.com>
2024-06-07 23:53:18 -0500

    look

--- Assets/Scripts/plamov.cs
+++ Assets/Scripts/plamov.cs
@@ -1,11 +1,11 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Plamov : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-
+    private Vector2 input;
     [Header("Configuración de Movimiento")]
     [SerializeField] private float velocidadMovimiento = 10f;
     [SerializeField] private float suavizadoMovimiento = 0.05f;
@@ -18,6 +18,15 @@ public class Plamov : MonoBehaviour
     [SerializeField] private LayerMask esSuelo;
     [SerializeField] private Transform Csuelo;
     [SerializeField] private Vector3 dimensionesCaja;
+
+
+    [Header("escalar")]
+    [SerializeField] private float velocidadEscalar;
+    private BoxCollider2D boxCollider2D;
+
+    private float gravedadInicial;
+    private bool escalando;
+
     private bool enSuelo;
 
     private bool salto = false;
@@ -25,11 +34,16 @@ public class Plamov : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        gravedadInicial = rb2d.gravityScale;
     }
 
     private void Update()
     {
+        input.y = Input.GetAxisRaw("Vertical");
+        input.x = Input.GetAxisRaw("Horizontal");
         movimientoHorizontal = Input.GetAxisRaw("Horizontal") * velocidadMovimiento;
+       
 
         if (Input.GetButtonDown("Jump") && enSuelo)
         {
@@ -41,6 +55,7 @@ public class Plamov : MonoBehaviour
     {
         enSuelo = Physics2D.OverlapBox(Csuelo.position, dimensionesCaja, 0f, esSuelo);
         Mover(movimientoHorizontal * Time.fixedDeltaTime, salto);
+        Escalar();
         salto = false;
     }
 
@@ -49,11 +64,11 @@ public class Plamov : MonoBehaviour
         Vector3 velocidadObjetivo = new Vector2(mover, rb2d.velocity.y);
         rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, velocidadObjetivo, ref velocidad, suavizadoMovimiento);
 
-        if (mover < 0 && !mirandoDerecha)
+        if (mover > 0 && mirandoDerecha)
         {
             Girar();
         }
-        else if (mover > 0 && mirandoDerecha)
+        else if (mover < 0 && !mirandoDerecha)
         {
             Girar();
         }
@@ -62,6 +77,26 @@ public class Plamov : MonoBehaviour
         {
             rb2d.AddForce(new Vector2(0f, fuerzaSalto));
             enSuelo = false;
+        }
+    }
+
+    private void Escalar()  
+    {
+        if ((input.y !=0 || escalando) && (boxCollider2D.IsTouchingLayers(LayerMask.GetMask("escaleras")))) {
+            Vector2 velocidadSubida = new Vector2(rb2d.velocity.x, input.y * velocidadEscalar);
+            rb2d.velocity = velocidadSubida;
+            rb2d.gravityScale = 0;
+            escalando = true;
+        }
+        else
+        {
+            rb2d.gravityScale = gravedadInicial;
+            escalando = false;
+        }
+
+        if (enSuelo)
+        {
+            escalando = false;
         }
     }
 
