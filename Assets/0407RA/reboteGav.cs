154b9976fed42ef3e572f5456430f789f3cf36f1
C.Ruiz <74808642+cruiz311@users.noreply.github.com>
2024-07-27 03:24:30 -0500

    Cambios Visuales

--- Assets/0407RA/reboteGav.cs
+++ Assets/0407RA/reboteGav.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class mrebote : MonoBehaviour
 {
-
-
-
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
